dd79743c2189b44ed967c0c7ec94105af4426da7
JackCT1 <jackthomas933@gmail.com>
2024-03-26 23:15:12 +0000

    retrieving file names in specified s3 bucket

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -15,4 +15,9 @@ SECRET_ACCESS_KEY = os.getenv("ACCESS_KEY")
 s3 = client('s3', aws_access_key_id = ACCESS_KEY_ID, aws_secret_access_key = SECRET_ACCESS_KEY)
 
 def download_relevant_files_from_s3():
+    """
+    Accesses relevant s3 bucket, downloads contents and puts into specified directory
+    """
+    contents = s3.list_objects(Bucket=BUCKET_NAME)["Contents"]
+    file_names = [list_object["Key"] for list_object in contents]
     return ''
