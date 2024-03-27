d62e7fdce063826cde4efe645f675c8ff2b8549e
JackCT1 <jackthomas933@gmail.com>
2024-03-27 14:50:34 +0000

    added datetime variables to get most recent file

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -14,13 +14,21 @@ SECRET_ACCESS_KEY = os.getenv("ACCESS_KEY")
 
 s3 = client('s3', aws_access_key_id = ACCESS_KEY_ID, aws_secret_access_key = SECRET_ACCESS_KEY)
 
-def download_relevant_files_from_s3(client: client, bucket: str, folder: str) -> bool:
+def download_relevant_files_from_s3(client: client, bucket: str, folder: str, today: datetime) -> bool:
     """
     Accesses relevant s3 bucket, downloads contents and puts into specified directory
     """
     contents = s3.list_objects(Bucket=bucket)["Contents"]
     file_names = [list_object["Key"] for list_object in contents]
+
+    current_year = today.year
+    current_month = today.month
+    current_day = today.day
+    current_hour = today.hour
+    
     files_downloaded = 0
+    for file in file_names:
+        files_downloaded += 1
     if files_downloaded > 0:
         return True
     return False
