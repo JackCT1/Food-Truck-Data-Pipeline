66c61f0341bbbc609a4777be5725045e761d2f9e
JackCT1 <jackthomas933@gmail.com>
2024-03-28 15:50:29 +0000

    finished function

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def combine_files_into_dataframe(filename: str, folder: str) -> bool:
     csv_files = [file for file in listdir(folder) if file.endswith(".csv")]
+    upload_csv_file = f"{folder}/{filename}.csv"
 
     for file in csv_files:
         number_id = (file.split("T3_T"))[1].split("_")[0]
@@ -12,6 +13,8 @@ def combine_files_into_dataframe(filename: str, folder: str) -> bool:
         remove(f"{folder}/{file}")
     
     merge_df.to_csv(f"{folder}/{filename}.csv", index=False)
+    if upload_csv_file in listdir(folder):
+        return True
 
     return False
 
