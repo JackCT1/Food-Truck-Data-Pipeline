38c47292ea9d244ef3969d79365608e9842b3706
JackCT1 <jackthomas933@gmail.com>
2024-03-28 15:46:57 +0000

    importing os modules and written concatenation function

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,7 +1,19 @@
+from os import listdir, path, remove
 import pandas as pd
 
-def combine_files_into_dataframe() -> pd.DataFrame:
-    return ''
+def combine_files_into_dataframe(filename: str, folder: str) -> bool:
+    csv_files = [file for file in listdir(folder) if file.endswith(".csv")]
+
+    for file in csv_files:
+        number_id = (file.split("T3_T"))[1].split("_")[0]
+        df = pd.read_csv(path.join(folder, file))
+        df['truck_id'] = number_id
+        merge_df = pd.concat([merge_df, df], ignore_index=True)
+        remove(f"{folder}/{file}")
+    
+    merge_df.to_csv(f"{folder}/{filename}.csv", index=False)
+
+    return False
 
 def clean_dataframe() -> pd.DataFrame:
     return ''
