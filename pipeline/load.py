3986c6a95e259353e640a2feab32542bf52d4fab
JackCT1 <jackthomas933@gmail.com>
2024-04-02 15:16:32 +0100

    written function to connect to remote database

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,5 +1,20 @@
-from os import environ
+import os
 import csv
 from dotenv import load_dotenv
 import redshift_connector
 from redshift_connector import Connection
+
+load_dotenv()
+
+def connect_to_database() -> Connection:
+    """
+    Connects tp relevant database
+    """
+
+    return redshift_connector.connect(
+     host=os.getenv["DB_HOST"],
+     database=os.getenv["DB_NAME"],
+     port=os.getenv["DB_PORT"],
+     user=os.getenv["DB_USERNAME"],
+     password=os.getenv["DB_PASSWORD"]
+     )
