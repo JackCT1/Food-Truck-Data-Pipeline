3b85b264ce54543284a0aed07672284616bd7188
JackCT1 <jackthomas933@gmail.com>
2024-04-03 23:29:32 +0100

    importing functions from etl files

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,3 +1,7 @@
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+
+from extract import download_relevant_files_from_s3
+from transform import combine_files_into_dataframe, clean_dataframe
+from load import connect_to_database, convert_csv_to_list, upload_transaction_data
