44a5a02949b1cd24f6bf1e9099686e59429dc46d
JackCT1 <jackthomas933@gmail.com>
2024-04-04 13:21:45 +0100

    loading credentials

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,3 +5,12 @@ import os
 from extract import download_relevant_files_from_s3
 from transform import combine_files_into_dataframe, clean_dataframe
 from load import connect_to_database, convert_csv_to_list, upload_transaction_data
+
+load_dotenv()
+
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+ACCESS_KEY_ID = os.getenv("ACCESS_ID")
+SECRET_ACCESS_KEY = os.getenv("ACCESS_KEY")
+
+if __name__ == "__main__":
+    ''
