3a148f4eb709a5ffe78202514310e48e1125d921
JackCT1 <jackthomas933@gmail.com>
2024-04-04 13:43:05 +0100

    importing client function and declaring as a variable

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 import os
 
-from extract import download_relevant_files_from_s3
+from extract import connect_to_s3, download_relevant_files_from_s3
 from transform import combine_files_into_dataframe, clean_dataframe
 from load import connect_to_database, convert_csv_to_list, upload_transaction_data
 
@@ -13,4 +13,4 @@ ACCESS_KEY_ID = os.getenv("ACCESS_ID")
 SECRET_ACCESS_KEY = os.getenv("ACCESS_KEY")
 
 if __name__ == "__main__":
-    ''
+    s3_client = connect_to_s3(ACCESS_KEY_ID, SECRET_ACCESS_KEY)
