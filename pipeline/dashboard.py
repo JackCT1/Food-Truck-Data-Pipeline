0ed5e762a22232c4248fff2c0cb072672f1964cd
JackCT1 <jackthomas933@gmail.com>
2024-04-09 13:10:46 +0100

    connecting to db and retrieving data

--- pipeline/dashboard.py
+++ pipeline/dashboard.py
@@ -1,4 +1,15 @@
-import streamlit as st
-import pandas as pd
 import altair as alt
 import numpy
+import pandas as pd
+import redshift_connector
+import streamlit as st
+
+from load import connect_to_database
+
+def get_dataframe(conn: redshift_connector.Connection, schema_name: str, db_name: str):
+    with conn.cursor() as cur:
+        cur.execute("SET SEARCH_PATH = {schema_name};")
+        cur.execute(f"SELECT * FROM {db_name};")
+        transaction_df = cur.get_dataframe()
+    
+    return transaction_df
